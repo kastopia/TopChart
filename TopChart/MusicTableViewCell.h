704b8cbfc17155337f651aac56d87a020c6d4b70
Terry Kihoon Kwon <kastopia@gmail.com>
2016-10-28 11:44:35 -0400

    create MusicTableVieCell

---
+++ TopChart/MusicTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  MusicTableViewCell.h
+//  TopChart
+//
+//  Created by Kihoon Kwon on 2016-10-28.
+//  Copyright © 2016 Terry Kwon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MusicTableViewCell : UITableViewCell
+
++ (NSString *)reuseIdentifier;
+
+@end
---
+++ TopChart/MusicTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  MusicTableViewCell.m
+//  TopChart
+//
+//  Created by Kihoon Kwon on 2016-10-28.
+//  Copyright © 2016 Terry Kwon. All rights reserved.
+//
+
+#import "MusicTableViewCell.h"
+
+@implementation MusicTableViewCell
+
+-(id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:reuseIdentifier];
+    return self;
+}
+
++ (NSString *)reuseIdentifier {
+    return NSStringFromClass([MusicTableViewCell class]);
+}
+
+@end
