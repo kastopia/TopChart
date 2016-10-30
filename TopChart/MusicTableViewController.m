215f09b2d051feb987b8d9ab8b238053ee2259b7
Terry Kihoon Kwon <kastopia@gmail.com>
2016-10-30 18:33:25 -0400

    implement toolbar in musictableviewcontroller that manages preview play

--- TopChart/MusicTableViewController.m
+++ TopChart/MusicTableViewController.m
@@ -6,12 +6,23 @@
 //  Copyright © 2016 Terry Kwon. All rights reserved.
 //
 
+@import AVFoundation;
+
 #import "APIManager.h"
 #import "CacheManager.h"
 #import "MusicTableViewController.h"
 #import "MusicTableViewCell.h"
 
 @interface MusicTableViewController ()
+
+@property (nonatomic, strong) NSCache * attributedTextCache;
+
+@property (nonatomic, strong) UIBarButtonItem * playBarButtonItem;
+@property (nonatomic, strong) UIBarButtonItem * pauseBarButtonItem;
+@property (nonatomic, strong) UIBarButtonItem * musicTitleBarButtonItem;
+@property (nonatomic, strong) UIBarButtonItem * musicImageBarButtonItem;
+@property (nonatomic, strong) NSIndexPath * currentlyPlayingMusicIndex;
+@property (nonatomic, strong) AVPlayer * musicPlayer;
 
 @end
 
@@ -20,14 +31,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = NSLocalizedString(@"Music", @"Music");
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
     
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    self.pauseBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemPause
+                                                                            target:self
+                                                                            action:@selector(pauseButtonPressed:)];
+    self.playBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemPlay
+                                                                            target:self
+                                                                            action:@selector(playButtonPressed:)];
+    self.musicImageBarButtonItem = [[UIBarButtonItem alloc] initWithImage:nil
+                                                                    style:UIBarButtonItemStylePlain
+                                                                   target:nil
+                                                                   action:nil];
+    self.musicImageBarButtonItem.enabled = NO;
+    self.musicTitleBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@""
+                                                                    style:UIBarButtonItemStylePlain
+                                                                   target:self
+                                                                   action:@selector(musicTitleBarButtonItemPressed:)];
+    self.musicTitleBarButtonItem.tintColor = [UIColor blackColor];
+    self.toolbarItems = @[ self.pauseBarButtonItem, self.musicImageBarButtonItem, self.musicTitleBarButtonItem ];
+
     [self.tableView registerClass:[MusicTableViewCell class]
            forCellReuseIdentifier:[MusicTableViewCell reuseIdentifier]];
     [self fetchTopChartAndReloadTableView];
+    
+    
+    // subscribe to avplayer item did play to end time notification
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(musicDidFinishPlaying:)
+                                                 name:AVPlayerItemDidPlayToEndTimeNotification
+                                               object:nil];
+}
+
+- (void)dealloc {
+    // unsubscribe from NSNotification
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 - (NSString *)topChartURL {
@@ -38,9 +75,92 @@
     return self.dataArray;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)playButtonPressed:(UIBarButtonItem *)sender {
+    if (self.musicPlayer.rate == 0) {
+        [self.musicPlayer seekToTime:kCMTimeZero];
+        [self.musicPlayer play];
+        [self setToolbarItems: @[ self.pauseBarButtonItem,
+                                  self.musicImageBarButtonItem,
+                                  self.musicTitleBarButtonItem ]
+                     animated:YES];
+    }
+}
+
+- (void)pauseButtonPressed:(UIBarButtonItem *)sender {
+    [self.navigationController setToolbarHidden:YES animated:YES];
+    if ( self.musicPlayer ) {
+        [self.musicPlayer pause];
+        self.musicPlayer = nil;
+    }
+    self.currentlyPlayingMusicIndex = nil;
+}
+
+- (void)musicTitleBarButtonItemPressed:(UIBarButtonItem *)sender {
+    // scroll tableview to currently playing music cell
+    [self.tableView scrollToRowAtIndexPath:self.currentlyPlayingMusicIndex
+                          atScrollPosition:UITableViewScrollPositionBottom
+                                  animated:YES];
+}
+
+- (void)setupToolBarItems {
+    if (!self.currentlyPlayingMusicIndex) {
+        return;
+    }
+    
+    NSDictionary * music = self.songs[self.currentlyPlayingMusicIndex.row];
+
+    NSString * musicTitle = music[@"im:name"][@"label"];
+    NSString * artistName = music[@"im:artist"][@"label"];
+
+    NSString * title = [NSString stringWithFormat:@"%@ - %@", artistName, musicTitle];
+    self.musicTitleBarButtonItem.title = title;
+
+    NSString * albumImageUrl = music[@"im:image"][0][@"label"];
+    UIImage * image = [[CacheManager sharedInstance] objectForKey:albumImageUrl];
+    if ( image ) {
+        // convert image to 24x24
+        CGSize imageSize = CGSizeMake(24, 24);
+        UIGraphicsBeginImageContextWithOptions(imageSize, NO, 0.0);
+        [image drawInRect:CGRectMake(0, 0, imageSize.width, imageSize.height)];
+        UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        // this line allows the image to be shown without being painted by the tint color
+        self.musicImageBarButtonItem.image = [newImage imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    }
+    else {
+        __weak typeof(self) __weakSelf = self;
+        [[CacheManager sharedInstance] saveImageFromURL:albumImageUrl completionHandler:^(UIImage * image){
+            [__weakSelf setupToolBarItems];
+        }];
+    }
+
+    NSString * audioUrlString = music[@"link"][1][@"attributes"][@"href"];
+    if ( self.musicPlayer ) {
+        if ( self.musicPlayer.currentItem ) {
+            AVURLAsset *currentPlayerAsset = (AVURLAsset *)self.musicPlayer.currentItem.asset;
+            if ([currentPlayerAsset.URL.absoluteString isEqualToString:audioUrlString]) {
+                // replay if paused
+                if (self.musicPlayer.rate == 0) {
+                    [self.musicPlayer seekToTime:kCMTimeZero];
+                    [self.musicPlayer play];
+                }
+            }
+            else {
+                [self.musicPlayer pause];
+            }
+        }
+    }
+
+    self.musicPlayer = [AVPlayer playerWithURL:[NSURL URLWithString:audioUrlString]];
+    [self.musicPlayer play];
+
+    [self setToolbarItems: @[ self.pauseBarButtonItem, self.musicImageBarButtonItem, self.musicTitleBarButtonItem ] animated:YES];
+    [self.navigationController setToolbarHidden:NO animated:YES];
+}
+
+- (void)musicDidFinishPlaying:(NSNotification *)notification {
+    NSLog(@"musicDidFinishPlaying");
+    [self setToolbarItems: @[ self.playBarButtonItem, self.musicImageBarButtonItem, self.musicTitleBarButtonItem ] animated:YES];
 }
 
 #pragma mark - Table view data source
@@ -64,24 +184,28 @@
     NSDictionary * music = self.songs[indexPath.row];
     NSString * albumImageUrl = music[@"im:image"][0][@"label"];
     
-    NSString * musicTitle = music[@"im:name"][@"label"];
-    NSString * artistName = music[@"im:artist"][@"label"];
-    NSString * albumTitle = music[@"im:collection"][@"im:name"][@"label"];
-    NSString * artistAndAlbumName = [NSString stringWithFormat:@"%@ - %@", artistName, albumTitle];
+    NSAttributedString * attributedText = [self.attributedTextCache objectForKey:indexPath];
+    if ( attributedText ) {
+        cell.contentLabel.attributedText = attributedText;
+    }
+    else {
+        NSString * musicTitle = music[@"im:name"][@"label"];
+        NSString * artistName = music[@"im:artist"][@"label"];
+        NSString * albumTitle = music[@"im:collection"][@"im:name"][@"label"];
+        NSString * artistAndAlbumName = [NSString stringWithFormat:@"%@ - %@", artistName, albumTitle];
 
-    NSString * attributedTextString = [NSString stringWithFormat:@"%@\n%@", musicTitle, artistAndAlbumName];
-    NSMutableAttributedString * attributedText = [[NSMutableAttributedString alloc] initWithString:attributedTextString];
-
-    [attributedText addAttributes:@{
-                                NSForegroundColorAttributeName: [UIColor grayColor],
-                                NSFontAttributeName: [UIFont systemFontOfSize:[UIFont smallSystemFontSize]],
-                            }
-                            range:[attributedTextString rangeOfString:artistAndAlbumName]];
+        NSString * attributedTextString = [NSString stringWithFormat:@"%@\n%@", musicTitle, artistAndAlbumName];
+        NSMutableAttributedString * attributedText = [[NSMutableAttributedString alloc] initWithString:attributedTextString];
+        [attributedText addAttributes:@{
+                                        NSForegroundColorAttributeName: [UIColor grayColor],
+                                        NSFontAttributeName: [UIFont systemFontOfSize:[UIFont smallSystemFontSize]],
+                                        }
+                                range:[attributedTextString rangeOfString:artistAndAlbumName]];
+        cell.contentLabel.attributedText = attributedText;
+        [self.attributedTextCache setObject:attributedText forKey:indexPath]; // store it to cache
+    }
 
     UIImage * image = [[CacheManager sharedInstance] objectForKey:albumImageUrl];
-
-    cell.contentLabel.attributedText = attributedText;
-
     if ( image ) {
         [cell setDownloadedImage:image];
     }
@@ -103,8 +227,14 @@
 }
 
 #pragma mark - Table view data source
+
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     return 75.0f;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    self.currentlyPlayingMusicIndex = indexPath;
+    [self setupToolBarItems];
 }
 
 /*
